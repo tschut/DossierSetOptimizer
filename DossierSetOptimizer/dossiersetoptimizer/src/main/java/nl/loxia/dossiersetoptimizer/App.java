5ff8a0471afcb3738c7863031a2c66682e6a10b5
tschut <tschut@gmail.com>
2013-12-19 20:07:55 +0100

    Java/Maven project aangemaakt

---
+++ DossierSetOptimizer/dossiersetoptimizer/src/main/java/nl/loxia/dossiersetoptimizer/App.java
@@ -0,0 +1,13 @@
+package nl.loxia.dossiersetoptimizer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
